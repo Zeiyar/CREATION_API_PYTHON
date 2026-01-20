6aaefb589c9e770eadc7cfe3213417fa80d11514
Zeiyar <marieycorentin97@gmail.com>
2026-01-20 09:13:17 +0100

    instruction script

---
+++ script.py
@@ -0,0 +1,8 @@
+#hors APIs
+
+#charger ticket JSON
+#compter ticket par status (status / ID)
+#filtrer les tickets (prio status / id)
+#trier tickets
+#ajouter ticket
+#mettre a jour le ticket existant
